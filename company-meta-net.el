0abcd6148fdea09dc7c65dce4d71a1aa78554873
Jen-Chieh Shen <jcs090218@gmail.com>
2022-07-04 14:31:23 +0800

    Update company-meta-net.el

--- company-meta-net.el
+++ company-meta-net.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/emacs-vs/company-meta-net
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "25.1") (company "0.8.12") (meta-net "1.1.0") (ht "2.3"))
-;; Keywords: csproj csharp company
+;; Keywords: convenience csproj csharp company
 
 ;; This file is NOT part of GNU Emacs.
 
